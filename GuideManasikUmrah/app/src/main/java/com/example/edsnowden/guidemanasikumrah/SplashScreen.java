ba943878625a0e21b2a85ae02ad82db55732b135
cahyadiws <cahyadiws666@gmail.com>
2017-10-14 20:51:57 +0700

    v1.1.1

--- GuideManasikUmrah/app/src/main/java/com/example/edsnowden/guidemanasikumrah/SplashScreen.java
+++ GuideManasikUmrah/app/src/main/java/com/example/edsnowden/guidemanasikumrah/SplashScreen.java
@@ -5,29 +5,31 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
-    
+public class SplashScreen extends Activity {
+
+    private static int splashInterval = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        progress_bar=(ProgressBar)findViewById(R.id.progressBar3);
 
-        Thread myThread = new Thread() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    sleep(3000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                this.finish();
             }
-        };
-        myThread.start();
-    }
+
+            private void finish() {
+
+            }
+        }, splashInterval);
+    };
 }
