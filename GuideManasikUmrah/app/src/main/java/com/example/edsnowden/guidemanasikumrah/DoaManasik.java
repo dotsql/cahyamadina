c371f9d5998f68352dddf46c85ae87efd34d4570
cahyadiws <cahyadiws666@gmail.com>
2017-10-14 23:01:13 +0700

    v1.1.2

---
+++ GuideManasikUmrah/app/src/main/java/com/example/edsnowden/guidemanasikumrah/DoaManasik.java
@@ -0,0 +1,13 @@
+package com.example.edsnowden.guidemanasikumrah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoaManasik extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doa_manasik);
+    }
+}
